aab650e95ba688a984852b9ee6828454f8db3ec6
canberksahin20@hotmail.com <canberksahin20@hotmail.com>
2021-02-24 19:36:54 +0300

    Add project files.

---
+++ IDistrubtedCacheRedisApp.Web/Controllers/Products.cs
@@ -0,0 +1,48 @@
+﻿using IDistrubtedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDistrubtedCacheRedisApp.Web.Controllers
+{
+    public class Products : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public Products(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            //await _distributedCache.SetStringAsync("surname.netcore", "Denemeee",cacheEntryOptions);
+
+            Product product = new Product { Id=1,Name="Product1",Price=9.99};
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            await _distributedCache.SetStringAsync("product:1",jsonProduct,cacheEntryOptions);
+
+            return View();
+        }
+
+        public async Task<IActionResult> Show()
+        {
+            
+            ViewBag.name = await _distributedCache.GetStringAsync("surname.netcore");
+            return View();
+        }
+
+        public async Task<IActionResult> Delete()
+        {
+            await _distributedCache.RemoveAsync("surname.netcore");
+
+            return View();
+        }
+    }
+}
